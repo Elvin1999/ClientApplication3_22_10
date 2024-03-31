b59ae80c43b8dc91cc94bb5ef4d0eff4187efe91
Elvin Jamalzade <camalzadeelvin@itstep.edu.az>
2024-03-31 11:13:18 +0400

    done

--- ClientApplication3_22_10/MainWindow.xaml.cs
+++ ClientApplication3_22_10/MainWindow.xaml.cs
@@ -14,7 +14,7 @@ namespace ClientApplication3_22_10
     /// </summary>
     public partial class MainWindow : Window
     {
-        private static readonly Socket ClientSocket=new Socket(AddressFamily.InterNetwork,
+        private static readonly Socket ClientSocket = new Socket(AddressFamily.InterNetwork,
             SocketType.Stream, ProtocolType.Tcp);
 
         private const int PORT = 27001;
@@ -45,9 +45,9 @@ namespace ClientApplication3_22_10
             var buffer = new byte[100000];
             int received = ClientSocket.Receive(buffer, SocketFlags.None);
             if (received == 0) return;
-            var data=new byte[received];
+            var data = new byte[received];
             Array.Copy(buffer, data, received);
-            string text=Encoding.ASCII.GetString(data);
+            string text = Encoding.ASCII.GetString(data);
 
             App.Current.Dispatcher.Invoke(() =>
             {
@@ -77,7 +77,7 @@ namespace ClientApplication3_22_10
             var data = new byte[received];
             Array.Copy(buffer, data, received);
 
-            string text=Encoding.ASCII.GetString(data);
+            string text = Encoding.ASCII.GetString(data);
 
             App.Current.Dispatcher.Invoke(() =>
             {
@@ -95,36 +95,55 @@ namespace ClientApplication3_22_10
 
                 foreach (var item in result)
                 {
-                    Button button=new Button();
+                    Button button = new Button();
                     button.FontSize = 22;
-                    button.Margin=new Thickness(0,10,0,0);
+                    button.Margin = new Thickness(0, 10, 0, 0);
                     button.Content = item;
                     button.Click += Button_Click;
                     commandsStackPanel.Children.Add(button);
                 }
             });
         }
+        TextBox textbox = new TextBox();
         public string SelectedCommand { get; set; }
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if(sender is Button bt)
+            if (sender is Button bt)
             {
-                var content=bt.Content.ToString();
+                var content = bt.Content.ToString();
                 var result = content.Remove(content.Length - 1, 1);
                 SelectedCommand = result;
 
                 var splitResult = result.Split('\\');
-                if (splitResult.Length > 2)
+                if (splitResult.Length > 2 || splitResult.Length == 2 && result.Contains("json"))
                 {
+                    textbox.Width = 800;
+                    textbox.Height = 60;
+                    textbox.Text = "*" + splitResult.Last();
+                    textbox.FontSize = 22;
 
+                    if (paramsStackPanel.Children.Count > 3)
+                    {
+                        paramsStackPanel.Children.RemoveAt(3);
+                        paramsStackPanel.Children.RemoveAt(3);
+                    }
+
+                    paramsStackPanel.Children.Add(textbox);
+
+                    Button button = new Button();
+                    button.FontSize = 22;
+                    button.Margin = new Thickness(0, 10, 0, 0);
+                    button.Content = "Execute";
+                    button.Click += ExecuteClick;
+                    paramsStackPanel.Children.Add(button);
                 }
                 else
                 {
-                    //if (paramsStackPanel.Children.Count > 3)
-                    //{
-                    //    paramsStackPanel.Children.RemoveAt(3);
-                    //    paramsStackPanel.Children.RemoveAt(3);
-                    //}
+                    if (paramsStackPanel.Children.Count > 3)
+                    {
+                        paramsStackPanel.Children.RemoveAt(3);
+                        paramsStackPanel.Children.RemoveAt(3);
+                    }
                     SendString(result);
                 }
 
@@ -132,9 +151,22 @@ namespace ClientApplication3_22_10
             }
         }
 
+        private void ExecuteClick(object sender, RoutedEventArgs e)
+        {
+            var result = SelectedCommand.Split('\\');
+            var resultText = result[0] + "\\" + result[1] + '\\' + textbox.Text;
+
+            if (SelectedCommand.Contains("json"))
+            {
+                var method = result[1].Split(' ')[0];
+                resultText = result[0] +'\\'+method+ ' ' + textbox.Text;
+            }
+            SendString(resultText);
+        }
+
         private void SendString(string result)
         {
-            byte[]buffer=Encoding.ASCII.GetBytes(result);
+            byte[] buffer = Encoding.ASCII.GetBytes(result);
             ClientSocket.Send(buffer, 0, buffer.Length, SocketFlags.None);
         }
 
